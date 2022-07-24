85fa8325f42d7c4e68a03595a813331c13bcf926
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2022-07-23 22:23:06 -0500

    Adding background logo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,55 @@
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
+import React, { useEffect, useState } from 'react'
+import Logo from './Logo'
 
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['y', 'a', 'n', ',']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+
+    // Uncomment useEffect to get text to animate when hovered
+    // breaking website right now when uncommented, need to fix bug
+
+
+    // useEffect(() => {
+    //     return setTimeout(() => {
+    //         setLetterClass('text-animate-hover')
+    //     }, 4000)
+    // }, [])
+
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm
-                    {/* <img src={LogoTitle} alt='developer' /> */}
-                    <span> R</span>yan,
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
                     <br />
-                    web developer
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    {/* <img src={LogoTitle} alt='developer' /> */}
+                    <span className='letter-logo-r'> R</span>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        index={15}
+                    />
+                    <br />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        index={22}
+                    />
                 </h1>
                 <h2>MongoDB | Express | React | Node</h2>
                 <Link to='/contact' className='flat-button'>CONTACT ME</Link>
             </div>
+            <Logo />
         </div>
     )
 }
