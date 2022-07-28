b81519667dc85c679e4de1726eb0dd29537dbe1f
Ryan McCutcheon <ryanmccutcheon@Ryans-MacBook-Air.local>
2022-07-28 15:16:12 -0500

    style fixes

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -9,12 +9,12 @@ const Layout = () => {
         <div className='App'>
             <Sidebar />
             <div className='page'>
-                <span className='tags top-tags'>&lt;body&gt;</span>
+                {/* <span className='tags top-tags'>&lt;body&gt;</span> */}
                 <Outlet />
-                <span className='tags bottom-tags'>&lt;/body&gt;
-                    <br />
-                    <span className='bottom-tag-html'>&lt;/html&gt;</span>
-                </span>
+                {/* <span className='tags bottom-tags'>&lt;/body&gt; */}
+                <br />
+                {/* <span className='bottom-tag-html'>&lt;/html&gt;</span> */}
+                {/* </span> */}
             </div>
             {/* <MouseParticles /> */}
             {/* <Hamburger /> */}
