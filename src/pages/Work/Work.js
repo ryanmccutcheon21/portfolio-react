630ab983caa7996cc40c48fc4a75e399c28c59bd
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2022-10-26 22:42:58 -0500

    Fixing image loading on work page

--- src/pages/Work/Work.js
+++ src/pages/Work/Work.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import '../../assets/images/TechFreak.png'
 import TechFreak from '../../assets/images/TechFreak.png'
 import LandingPage from '../../assets/images/LandingPage.png'
@@ -7,7 +7,8 @@ import UA from '../../assets/images/UA.png'
 import Hoobank from '../../assets/images/Hoobank.png'
 import RealEstate from '../../assets/images/RealEstate.png'
 import eCommerce from '../../assets/images/eCommerce.png'
-import Projects from './Projects/Projects';
+// import Projects from './Projects/Projects';
+import { ColorRing } from 'react-loader-spinner'
 
 
 const projects = [
@@ -64,21 +65,56 @@ const projects = [
 
 const Work = () => {
 
-    const projectList = projects?.map((project) =>
-        <Projects
-            name={project.name}
-            image={project.image}
-            description={project.description}
-            link={project.link}
-            id={project.id}
-            key={project.id}
-        />
-    );
+    const [loading, setLoading] = useState(true)
+    const counter = useRef(0)
+    const imageLoaded = () => {
+        counter.current += 1
+        if (counter.current >= projects.length) {
+            setLoading(false)
+        }
+    }
+
+    // const projectList = projects?.map((project) =>
+    //     <Projects
+    //         name={project.name}
+    //         image={project.image}
+    //         description={project.description}
+    //         link={project.link}
+    //         id={project.id}
+    //         key={project.id}
+    //         onLoad={imageLoaded}
+    //     />
+    // );
 
 
     return (
-        <div className='container overflow-scroll grid md:grid-cols-3'>
-            {projectList}
+        <div>
+            <div style={{ display: loading ? 'block' : 'none' }}>
+                <ColorRing
+                    visible={true}
+                    height="80"
+                    width="80"
+                    ariaLabel="blocks-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="blocks-wrapper"
+                    colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+                />
+            </div>
+            <div style={{ display: loading ? 'none' : 'block' }} className='container overflow-scroll'>
+                <div className='grid md:grid-cols-3 mx-16'>
+                    {projects.map(project => (
+                        <div className='mx-5'>
+                            <h2 className='text-[#ffd700] font-[Coolvetica] text-[2rem] mb-5 flex justify-center'>
+                                {project.name}
+                            </h2>
+                            <a href={project.link} target='_blank' className='hover:opacity-75'>
+                                <img src={project.image} onLoad={imageLoaded} />
+                            </a>
+                            <p className='text-white mt-5 flex justify-center'>{project.description}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
