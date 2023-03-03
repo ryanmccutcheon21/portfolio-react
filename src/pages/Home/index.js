aa2e93ede75b14d3b91323b60d1d1699469f0e93
Ryan McCutcheon <ryanmccutcheon5@yahoo.com>
2023-03-02 22:00:20 -0600

    Update Home to eliminate reused code and follow DRY approach

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,16 @@ import './index.scss'
 import AnimatedLetters from '../../components/AnimatedLetters'
 import React, { useState } from 'react'
 
+const TextAnimation = ({ letterClass, index, strArray }) => {
+    return strArray.map((letter, i) => {
+        const spanClass = `${letterClass}_${index + i} text-animate-hover`
+        return (
+            <span key={i} className={spanClass}>
+                {letter}
+            </span>
+        )
+    })
+}
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -12,29 +22,22 @@ const Home = () => {
     return (
         <div className="container home-page overflow-scroll">
             <div className="text-zone">
-                <h1 className='md:text-[5rem]'>
-                    <span className={`${letterClass}_11 text-animate-hover`}>H</span>
-                    <span className={`${letterClass}_12 text-animate-hover`}>i</span>
-                    <span className={`${letterClass}_12 text-animate-hover`}>,</span>
+                <h1 className="md:text-[5rem]">
+                    <TextAnimation letterClass={letterClass} index={11} strArray={['H', 'i', ',']} />
                     <br />
-                    <span className={`${letterClass}_13 text-animate-hover`}>I</span>
-                    <span className={`${letterClass}_14 text-animate-hover`}>'</span>
-                    <span className={`${letterClass}_14 text-animate-hover`}>m</span>
-                    <span className={`letter-logo-r text-animate-hover`}> R</span>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={nameArray}
-                        index={15}
-                    />
+                    <TextAnimation letterClass={letterClass} index={13} strArray={['I', "'", 'm']} />
+                    <span className="letter-logo-r text-animate-hover">R</span>
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} index={15} />
                     <br />
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={jobArray}
-                        index={22}
-                    />
+                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} index={22} />
                 </h1>
-                <h2 className='pt-[2rem]'>MongoDB | Express | React | Node</h2>
-                <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+                <h2 className="pt-[2rem]">MongoDB | Express | React | Node</h2>
+                <Link
+                    to="/contact"
+                    className="text-[#ffd700] text-[1.5rem] font-normal tracking-[.5rem] font-sans py-4 px-20 border border-[#ffd700] mt-20 animate-fadeInAnimation hover:bg-[#ffd700] hover:text-gray-700"
+                >
+                    CONTACT ME
+                </Link>
             </div>
         </div>
     )
